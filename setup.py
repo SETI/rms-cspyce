66f1aba54996e51517e1cc561427cedab756905a
Frank Yellin <fy@fyellin.com>
2022-03-13 23:02:01 -0700

    Get code to work on Windows.

--- setup.py
+++ setup.py
@@ -13,16 +13,20 @@
 try:
     from setuptools import Command, setup, Extension
     from setuptools.command.build_py import build_py
-except:
+except ImportError:
     from distutils.core import Command, setup, Extension
     from distutils.core import setup, Extension, build_py
 
-import numpy
-from glob import glob
+import os
 import subprocess
 import sys
+from glob import glob
+
+import numpy
 
 PYTHON2 = sys.version_info[0] < 3
+IS_WINDOWS = os.name == 'nt'
+
 
 class GenerateCommand(Command):
     description = 'Create generated files'
@@ -40,56 +44,73 @@ class GenerateCommand(Command):
             from swig.make_vectorize import create_vectorize_header_file
             create_vectorize_header_file("swig/vectorize.i")
             print("Rerunning swig")
-            command = "swig -python -outdir cspyce/. -o swig/cspyce0_wrap.c swig/cspyce0.i".split(' ')
+            command = "swig -python -outdir cspyce/. " \
+                      "-o swig/cspyce0_wrap.c swig/cspyce0.i".split(' ')
             subprocess.check_call(command)
-            command = "swig -python -outdir cspyce/. -o swig/typemap_samples_wrap.c swig/typemap_samples.i".split(' ')
+            command = "swig -python -outdir cspyce/. " \
+                      "-o swig/typemap_samples_wrap.c swig/typemap_samples.i".split(' ')
             subprocess.check_call(command)
         else:
             command = "echo Cannot rebuild files in Python2".split(' ')
             subprocess.check_call(command)
 
-cspice_module = Extension(
-    'cspyce.cspice',
-    sources=glob("cspice/src/cspice/*.c"),
-    include_dirs=['cspice/include'],
-    extra_compile_args=['-Wno-incompatible-pointer-types', '-Wno-parentheses', 
-               '-Wno-implicit-int', "-Wno-shift-op-parentheses",
-               '-Wno-logical-op-parentheses', '-Wno-sign-compare',
-               '-Wno-pointer-to-int-cast', '-Wno-strict-prototypes'],
-)
 
-lib_cspice = ("cspice", {
-    "sources" : glob("cspice/src/cspice/*.c"),
-    "include_dirs": ["cspice/include"],
-    "cflags": ['-Wno-incompatible-pointer-types', '-Wno-parentheses',
-               '-Wno-implicit-int', "-Wno-shift-op-parentheses",
-               '-Wno-logical-op-parentheses', '-Wno-sign-compare',
-               '-Wno-pointer-to-int-cast', '-Wno-strict-prototypes']
-})
+if not IS_WINDOWS:
+    cspice_cflags = ['-Wno-incompatible-pointer-types', '-Wno-parentheses',
+                     '-Wno-implicit-int', "-Wno-shift-op-parentheses",
+                     '-Wno-logical-op-parentheses', '-Wno-sign-compare',
+                     '-Wno-pointer-to-int-cast', '-Wno-strict-prototypes']
+    lib_cspice = ("cspice", {
+        "sources": glob("cspice/src/cspice/*.c"),
+        "include_dirs": ["cspice/include",],
+        "cflags": cspice_cflags,
+     })
+    cspice_libraries = [lib_cspice]
+else:
+    # The Windows linker cannot seem to handle 2400 files at once.  We split the
+    # files into 5 equal sized groups, and build five libraries.
+    splits = 5
+    files = glob("cspice/src/cspice/*.c")
+    cspice_libraries = [
+        ("cspice_" + str(i + 1), {
+            "sources": files[i::splits],
+            "include_dirs": ["cspice/include", ],
+            # These seem to be the flags that Windows requires
+            "cflags": ['-DKR_headers', '-DMSDOS', '/nowarn'],
+        })
+        for i in range(splits)]
+
+
+if IS_WINDOWS:
+    cspyce_cflags = ['/nowarn']
+else:
+    cspyce_cflags = ['-Wno-incompatible-pointer-types']
+
 
 cspyce0_module = Extension(
     'cspyce._cspyce0',
     sources=['swig/cspyce0_wrap.c'],
     include_dirs=['cspice/include', numpy.get_include()],
-    extra_compile_args=['-Wno-incompatible-pointer-types'],
+    extra_compile_args=cspyce_cflags,
 )
 
 typemap_samples_module = Extension(
     'cspyce._typemap_samples',
     sources=['swig/typemap_samples_wrap.c'],
     include_dirs=['cspice/include', numpy.get_include()],
-    extra_compile_args=['-Wno-incompatible-pointer-types'],
+    extra_compile_args=cspyce_cflags,
 )
 
-setup(name = 'cspyce',
-       version = '2.0.0',
-       author  = "Mark Showalter/PDS Ring-Moon Systems Node",
-       description = "Low-level SWIG interface to the CSPICE library",
-       ext_modules = [cspyce0_module, typemap_samples_module],
-       libraries=[lib_cspice],
-       packages=["cspyce"],
-       install_requires=['numpy'],
-       cmdclass={
-           'generate': GenerateCommand,
-       }
+setup(
+    name='cspyce',
+    version='2.0.0',
+    author="Mark Showalter/PDS Ring-Moon Systems Node",
+    description="Low-level SWIG interface to the CSPICE library",
+    ext_modules=[cspyce0_module, typemap_samples_module],
+    libraries=cspice_libraries,
+    packages=["cspyce"],
+    install_requires=['numpy'],
+    cmdclass={
+        'generate': GenerateCommand,
+    }
 )
