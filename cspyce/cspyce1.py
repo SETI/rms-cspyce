412bd28ecec40ba92a3b76c6fbf2af3c5cbdf58f
Frank Yellin <fy@fyellin.com>
2023-05-31 13:42:12 -0700

    Add comment explaining hack.

--- cspyce/cspyce1.py
+++ cspyce/cspyce1.py
@@ -1242,6 +1242,10 @@ for name in CSPYCE_BASENAMES:
 #     globals()[vfname] = vfunc
 
 
+# These are temporary variables used by cspyce1.py whose values happen to be
+# functions with an ARGNAMES property.  We don't want cspyce2.py to think that
+# these are actual functions it needs to know about.  So e remove them from the
+# namespace.
 
 del func, efunc, vfunc, vefunc
 
